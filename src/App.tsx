0530d2df5b2ed0f538899c53bc5da1ddf7a7579b
Matheus Vicente <matheusfsv98@gmail.com>
2025-10-30 17:36:20 -0300

    Estrutura básica

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import CalendarBlank from "./assets/icons/calendar-blank.svg?react";
+import CaretDown from "./assets/icons/caret-down.svg?react";
+import CaretLeft from "./assets/icons/caret-left.svg?react";
+import CaretRight from "./assets/icons/caret-right.svg?react";
+import CloudSun from "./assets/icons/cloud-sun.svg?react";
+import MoonStars from "./assets/icons/moon-stars.svg?react";
+import SunHorizon from "./assets/icons/sun-horizon.svg?react";
+import Trash from "./assets/icons/trash.svg?react";
+import UserSquare from "./assets/icons/user-square.svg?react";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="flex items-center gap-2">
+        <CalendarBlank className="text-gray-100 h-5 w-5" />
+        <CaretDown className="text-gray-100 h-5 w-5" />
+        <CaretLeft className="text-gray-100 h-5 w-5" />
+        <CaretRight className="text-gray-100 h-5 w-5" />
+        <CloudSun className="text-gray-100 h-5 w-5" />
+        <MoonStars className="text-gray-100 h-5 w-5" />
+        <SunHorizon className="text-gray-100 h-5 w-5" />
+        <Trash className="text-gray-100 h-5 w-5" />
+        <UserSquare className="text-gray-100 h-5 w-5" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
